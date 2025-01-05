992486d117e85e737dd49ceae997af9606f75c28
Skyler Mayfield <skyler544@gmail.com>
2025-01-05 12:22:56 +0100

    add validation for the timestamp

--- src/Domain/Time/TimeStamp.php
+++ src/Domain/Time/TimeStamp.php
@@ -8,6 +8,7 @@ class TimeStamp
         private readonly int $hour,
         private readonly int $minute,
     ) {
+        $this->validateTimeStamp($hour, $minute);
     }
 
     public function hour(): int
@@ -18,5 +19,24 @@ class TimeStamp
     public function minute(): int
     {
         return $this->minute;
+    }
+
+    private function validateTimeStamp(int $hour, int $minute): void
+    {
+        if (! $this->isValid($hour, $minute)) {
+            throw new \InvalidArgumentException(
+                "Invalid Martian timestamp: {$hour}:{$minute}"
+            );
+        }
+    }
+
+    private function isValid(int $hour, int $minute): bool
+    {
+        return $hour >= 0 && $hour < 25 && $minute >= 0 && $minute < 100;
+    }
+
+    public function __toString(): string
+    {
+        return sprintf('%02d:%02d', $this->hour(), $this->minute());
     }
 }
