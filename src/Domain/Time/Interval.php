9226d292ed1eac11e56e0b6663db8c41986d6ec7
Skyler Mayfield <skyler544@gmail.com>
2025-01-05 15:23:59 +0100

    set up duration calculation

--- src/Domain/Time/Interval.php
+++ src/Domain/Time/Interval.php
@@ -20,6 +20,22 @@ class Interval
         return $this->end;
     }
 
+    public function getDuration(): int
+    {
+        $startMinutes = $this->start->toMinutes();
+        $endMinutes = $this->adjustedEndTime($startMinutes);
+
+        return $endMinutes - $startMinutes;
+    }
+
+    private function adjustedEndTime(int $startMinutes): int
+    {
+        $endMinutes = $this->end->toMinutes();
+        return $endMinutes < $startMinutes
+            ? $endMinutes + 2500
+            : $endMinutes;
+    }
+
     public function __toString(): string
     {
         return "[{$this->start}, {$this->end}]";
--- tests/Domain/Time/IntervalTest.php
+++ tests/Domain/Time/IntervalTest.php
@@ -9,6 +9,32 @@ use PHPUnit\Framework\TestCase;
 class IntervalTest extends TestCase
 {
     /**
+     * @return array<int, array{TimeStamp, TimeStamp, int}>
+     */
+    public static function intervalDurationProvider(): array
+    {
+        return [
+            [new TimeStamp(0, 0), new TimeStamp(1, 0), 100],
+            [new TimeStamp(0, 0), new TimeStamp(0, 1), 1],
+            [new TimeStamp(10, 30), new TimeStamp(12, 30), 200],
+            [new TimeStamp(10, 30), new TimeStamp(12, 15), 185],
+            [new TimeStamp(23, 50), new TimeStamp(00, 30), 180],
+            [new TimeStamp(22, 45), new TimeStamp(01, 15), 370],
+            [new TimeStamp(24, 99), new TimeStamp(00, 00), 1],
+        ];
+    }
+
+    #[\PHPUnit\Framework\Attributes\DataProvider('intervalDurationProvider')]
+    public function testGetDuration(
+        TimeStamp $start,
+        TimeStamp $end,
+        int $expectedDuration
+    ): void {
+        $interval = new Interval($start, $end);
+        $this->assertEquals($expectedDuration, $interval->getDuration());
+    }
+
+    /**
      * @return array<int, array{TimeStamp, TimeStamp}>
      */
     public static function intervalProvider(): array
